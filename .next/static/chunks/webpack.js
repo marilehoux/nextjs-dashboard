f16f048159b676f02be72bd098aba3bb264e06d9
marilehoux <mari.lehoux@gmail.com>
2024-06-12 16:20:19 +0200

    loigin

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8a13a2546cfc4a75"; }
+/******/ 		__webpack_require__.h = function() { return "f0d1a6bfaa8ebeeb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,9 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string({
@@ -92,4 +95,23 @@ export async function updateInvoice(id: string, formData: FormData) {
 	} catch (error) {
 		return { message: 'Database error: invoice not deleted' };
 	  }
+  }
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
   }
